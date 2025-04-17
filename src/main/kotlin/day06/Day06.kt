7c1dfe721cb1bd1a30f5c2ce0856cd5812a9dfa2
Hiep Pham <hiepph9@proton.me>
2025-04-17 16:21:51 +0200

    Solve day 06 part 1

---
+++ src/main/kotlin/day06/Day06.kt
@@ -0,0 +1,26 @@
+package day06
+
+import printResult
+import readInput
+
+object Day06 {
+    fun part1(input: String): Int {
+        return findMarker(input, 4)
+    }
+
+    private fun findMarker(input: String, numberOfDifferentCharacters: Int): Int {
+        for (i in 0..input.length - numberOfDifferentCharacters) {
+            val window = input.substring(i, i + numberOfDifferentCharacters)
+            if (window.toSet().size == numberOfDifferentCharacters) {
+                return i + numberOfDifferentCharacters
+            }
+        }
+
+        return -1
+    }
+}
+
+fun main() {
+    val input = readInput("Day06")
+    printResult("1", Day06.part1(input))
+}
---
+++ src/test/kotlin/Day06Test.kt
@@ -0,0 +1,18 @@
+import day06.Day06
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class Day06Test {
+    val testInputs = readLines("Day06", env = "test")
+    val input = readInput("Day06")
+
+    @Test
+    fun testPart1() {
+        val testResults = listOf(7, 5, 10)
+        (testResults zip testInputs).forEach { (expected, testInput) ->
+            assertEquals(expected = expected, actual = Day06.part1(testInput))
+        }
+
+        assertEquals(expected = 1965, actual = Day06.part1(input))
+    }
+}
